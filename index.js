45ebae6c51fbcd26bdf0f0ca3a83ac57c6f33c68
dylan <runnels.dylan@yahoo.com>
2024-04-20 13:03:15 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+
+let num3 = 20
+let num4 = 8
+
+let mod = num3 % num4;
+
+let max = Math.max(10, 5, 20, 15);
